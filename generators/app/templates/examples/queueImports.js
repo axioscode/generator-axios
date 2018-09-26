95b74d5b0247d47fb6344da7a8c2c495ce03c026
geraldarthur <gerald.a.rich@gmail.com>
2018-09-26 16:05:08 -0400

    add async to queue

--- generators/app/templates/examples/queueImports.js
+++ generators/app/templates/examples/queueImports.js
@@ -1,8 +1,8 @@
 import csvFile1 from "../data/csvFile1.csv";
 import csvFile2 from "../data/csvFile2.csv";
 
-function queueImports() {
-  let [data1, data2] = Promise.all([
+async function queueImports() {
+  let [data1, data2] = await Promise.all([
     d3.csv(csvFile1),
     d3.csv(csvFile2)
   ]);
