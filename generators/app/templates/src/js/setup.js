d317e524f3489877e5168cf36503f5f3702e3bb6
geraldarthur <gerald.a.rich@gmail.com>
2018-10-23 22:47:05 -0400

    add ie11 polyfills for fetch & Array.includes

--- generators/app/templates/src/js/setup.js
+++ generators/app/templates/src/js/setup.js
@@ -4,6 +4,7 @@ import "core-js/fn/object/entries";
 import "core-js/fn/object/values";
 import "core-js/fn/array/includes";
 import "raf/polyfill";
+import "whatwg-fetch";
 
 import {
   setupVisualsGoogleAnalytics
