8ca395bd53c1e4e32a1632f6014b57e3f4de692b
geraldarthur <gerald.a.rich@gmail.com>
2019-01-10 14:04:21 -0500

    update deps; use new babel plugins; update webpack config

---
+++ generators/app/templates/babel.config.js
@@ -0,0 +1,24 @@
+module.exports = api => {
+  api.cache.invalidate(() => process.env.NODE_ENV === "production");
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          targets: {
+            browsers: ["last 2 versions", "Safari 9", "IE 11"],
+          },
+          useBuiltIns: "entry",
+          modules: false,
+          exclude: ["transform-regenerator"],
+        },
+      ],
+    ],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-syntax-dynamic-import",
+      "@babel/plugin-transform-async-to-generator",
+      "@babel/plugin-transform-runtime",
+    ],
+  };
+};
