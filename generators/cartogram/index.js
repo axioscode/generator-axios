4e91cb6e136f353540ade5497c00ee7f37571ab9
Michelle McGhee <michmac202@gmail.com>
2021-02-09 16:08:29 -0600

    remove console logs

--- generators/cartogram/index.js
+++ generators/cartogram/index.js
@@ -9,7 +9,6 @@ module.exports = class extends Generator {
 
   // 1. Start the axios generator in parallel
   initializing() {
-    console.log("initializing - cartogram");
     this.composeWith({
       Generator: AxiosGenerator,
       path: require.resolve("../app")
@@ -18,7 +17,6 @@ module.exports = class extends Generator {
 
   // 2. After axios generator has copied its files, replace the src folder
   default() {
-    console.log("default - cartogram");
     this.fs.delete(this.destinationPath("src/**"));
     this.fs.copy(this.templatePath("src/**"), this.destinationPath("src"), {
       globOptions: {}
@@ -28,7 +26,6 @@ module.exports = class extends Generator {
   // 3. Extend the dependencies to include any new ones from package.json
   // (This happens right before the axios generator does `yarn install`)
   install() {
-    console.log("install - cartogram");
     const newDeps = {
       dependencies: packages.dependencies,
       devDependencies: packages.devDependencies
